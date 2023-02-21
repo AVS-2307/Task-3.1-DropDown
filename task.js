2cbd76b7de803382d44d85fe914a1c130b2d3a16
AVS-2307 <andrey230781.an@gmail.com>
2023-02-21 20:50:44 +0300

    event delegation for links

--- task.js
+++ task.js
@@ -1,20 +1,18 @@
 const dropDownValue = document.querySelector('.dropdown__value');
 const dropDownList = document.querySelector('.dropdown__list');
-const dropDownItems = Array.from(document.querySelectorAll('.dropdown__link'));
 
 dropDownValue.addEventListener('click', function() {
-    dropDownList.classList.add('dropdown__list_active');
-    
+    dropDownList.classList.add('dropdown__list_active');    
 });
 
-function chooseItem() {
-    dropDownItems.forEach(item => {
-        item.addEventListener('click', function(event) {
-            dropDownValue.textContent = item.textContent;
+function chooseItem() { 
+    //ловим клик на всплытии, не на dropdown__link, а на родителе    
+    dropDownList.addEventListener('click', function(event) {            
+            dropDownValue.textContent = event.target.textContent; //присваиваем название по клику на target;
             dropDownList.classList.remove('dropdown__list_active');
             event.preventDefault();
             });       
-    });
+
 };
 
 chooseItem();
